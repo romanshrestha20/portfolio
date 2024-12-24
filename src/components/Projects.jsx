243ddf1738f82c528e505419ef1ba3de4c5d24cf
Roman Shrestha <stha.roman20@gmail.com>
2024-12-25 01:47:02 +0200

    Update project image URL to use a direct link from Unsplash

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -23,7 +23,7 @@ const projects = [
     name: "Another Project",
     description:
       "Another example description, which is short and doesn't need an expand option.",
-    image: "/images/profile.jpg",
+    image: "https://plus.unsplash.com/premium_photo-1682656220562-32fde8256295?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     link: "https://github.com/your-profile/project-repo",
   },
 ];
