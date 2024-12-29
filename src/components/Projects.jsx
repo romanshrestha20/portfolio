1de95523ab83485729b3083e60b346e57281e841
Roman Shrestha <stha.roman20@gmail.com>
2024-12-29 12:50:07 +0200

    Update project image URL in Projects component for My Book Library

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -24,7 +24,7 @@ const projects = [
     description:
       "A web app for managing books and authors, including adding, viewing, editing, and deleting records, with user authentication and profiles for personalized experiences.",
     image:
-      "https://plus.unsplash.com/premium_photo-1682656220562-32fde8256295?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      "https://github.com/romanshrestha20/portfolio/blob/main/public/Screenshot_29-12-2024_123130_localhost.jpeg?raw=true",
     link: "https://github.com/romanshrestha20/my-book-library",
   },
 ];
