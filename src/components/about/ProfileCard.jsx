4b06d73702074f9e6b9d35b5d89c29251c22cda9
romanshrestha20 <stha.roman20@gmail.com>
2025-12-21 14:54:52 +0200

    Fix border color in ProfileCard component for consistency

--- src/components/about/ProfileCard.jsx
+++ src/components/about/ProfileCard.jsx
@@ -5,11 +5,11 @@ import { Download } from "lucide-react";
 export default function AboutText() {
   return (
     <>
-      <motion.div className="relative w-56 h-56 mb-6">
+      <motion.div className="relative w-56 h-56 mb-6 ">
         <motion.img
           src="https://images.unsplash.com/profile-1759063902941-781bafd762a3image?w=150&dpr=2&crop=faces&bg=%23fff&h=150&auto=format&fit=crop&q=60&ixlib=rb-4.1.0"
           alt="Profile Picture"
-          className="absolute inset-0 object-cover w-full h-full transition-transform transform border-4 rounded-full shadow-lg border-primary dark:border-blue-400 hover:scale-105"
+          className="absolute inset-0 object-cover w-full h-full transition-transform transform border-4 rounded-full shadow-lg border-dark hover:scale-105"
         />
       </motion.div>
 
