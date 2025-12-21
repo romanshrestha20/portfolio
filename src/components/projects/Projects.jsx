4f81625bd71f9e3838f7baf5ed71a89322a7fb66
romanshrestha20 <stha.roman20@gmail.com>
2025-12-21 15:18:13 +0200

    Add new projects to Projects component and adjust layout for consistency

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -11,6 +11,14 @@ const projects = [
     link: "https://github.com/romanshrestha20/django-blog",
   },
   {
+    name: "Django Blog",
+    description:
+      "A web app for creating, reading, updating, and deleting blog posts with user authentication and profiles.",
+    image:
+      "https://github.com/romanshrestha20/portfolio/blob/main/public/Screenshot_25-12-2024_11926_127.0.0.1.jpeg?raw=true",
+    link: "https://github.com/romanshrestha20/django-blog",
+  },
+  {
     name: "Movie Application",
     description:
       "Keeps users updated with the latest movies and TV shows, featuring popular and trending titles.",
@@ -27,24 +35,32 @@ const projects = [
     link: "https://github.com/romanshrestha20/my-book-library",
   },
   {
-    name: " CleanSync",
+    name: "CleanSync",
     description:
       "CleanSync is a modern Android app built with Kotlin and Jetpack Compose, designed for seamless user management, booking services, and notifications.",
     image: "https://github.com/Mobile-Development-Project-Group-22/CleanSync/blob/main/Screenshot_20250428_135436.png?raw=true",
-    link: "https://github.com/Mobile-Development-Project-Group-22/CleanSync/"
-  }
+    link: "https://github.com/Mobile-Development-Project-Group-22/CleanSync/",
+  },
+  {
+    name: "Hamro Pasal",
+    description:
+      "An e-commerce platform for buying and selling products online with secure transactions and user-friendly interface.",
+    image:
+      "https://github.com/romanshrestha20/portfolio/blob/main/public/hamro-pasal.jpeg?raw=true",
+    link: "https://hamro-pasal-frontend-1dqm.vercel.app/",
+  },
 ];
 
 export default function Projects() {
   return (
     <section
       id="projects"
-      className="py-16 bg-background dark:bg-gray-900 text-gray-900 dark:text-white"
+      className="py-16 text-gray-900 bg-background dark:bg-gray-900 dark:text-white"
     >
-      <div className="container mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="container px-4 mx-auto sm:px-6 lg:px-8">
         <SectionHeader title="My Projects" subtitle="Explore some of the work I've done" />
 
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+        <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
           {projects.map((project, i) => (
             <ProjectCard key={i} project={project} />
           ))}
